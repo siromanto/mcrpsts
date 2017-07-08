7d454e0b44aa00c6b9b31a33e973797909c14084
siromanto <dmytrenko.serhii@gmail.com>
2017-07-08 13:50:14 +0300

    changing app port to 3002

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,14 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Change localhost port to 3002
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 3002)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
